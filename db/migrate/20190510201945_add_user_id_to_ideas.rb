66efaee8c84dce13cbab74efb43a73fd75018a37
Steve Chen <stevechen@FB-MacbookPro-07.local>
2019-06-12 11:21:23 -0400

    Authorization on ideas

--- db/migrate/20190510201945_add_user_id_to_ideas.rb
+++
@@ -1,6 +0,0 @@
-class AddUserIdToIdeas < ActiveRecord::Migration[5.2]
-  def change
-    add_column :ideas, :user_id, :integer
-    add_index :ideas, :user_id
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_10_201945) do
+ActiveRecord::Schema.define(version: 2019_06_12_144245) do
 
   create_table "ideas", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 2019_05_10_201945) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.integer "user"
+    t.index ["user"], name: "index_ideas_on_user"
     t.index ["user_id"], name: "index_ideas_on_user_id"
   end
 
